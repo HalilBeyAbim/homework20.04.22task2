8b9cfe641036e24c31d6ea7d177ebdfd6c90c9a0
HalilBeyAbim <khalilbaygkh@code.edu.az>
2022-04-20 17:24:39 +0400

    added new home work

---
+++ HomeWork20.04.22_Task2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace HomeWork20._04._22_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("Xelilbey", "Xelilbeyli", 28, "Male", true, 75);
+            Console.WriteLine(student.GetInfo());
+        }
+
+    }
+    class Student
+    {
+        public string Name;
+        public string Surname;
+        public byte Age;
+        public string Gender;
+        public bool isGraduated;
+        public byte Point;
+        public Student(string name, string surname, byte age, string gender, bool graduated, byte point)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+            Gender = gender;
+            Point = point;
+            isGraduated = graduated;
+            if (graduated = true)
+            {
+                Console.WriteLine("Imtahannan kecdiniz");
+            }
+            if (point >= 80)
+            {
+                Console.WriteLine("ikinci imtahana kecemeye sans qazandiz");
+            }
+            else
+            {
+                Console.WriteLine("imtahannan kesildiniz");
+            }
+        }
+        public string GetInfo()
+        {
+
+            return $"Name:{Name} Surname: {Surname} Age: {Age} Gender: {Gender} Point: {Point} Is Graduated: {isGraduated} ";
+
+        }
+    }
+}
